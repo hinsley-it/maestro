83a847a8a80e5b50a191b56e9833899f8e867f9f
Carter Hinsley <carterhinsley@gmail.com>
2015-08-05 15:40:05 -0400

    Set up Qunit test suite

---
+++ tests/hello_world.js
@@ -0,0 +1,4 @@
+QUnit.test('Hello, world!', function (assert) {
+    var value = 'Hello, world!';
+    assert.equal(value, 'Hello, world!', '`value` is expected to equal the string "Hello, world!".');
+});
