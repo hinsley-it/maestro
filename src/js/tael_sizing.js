2e4fa079ee4b0a870ce3e1a530ffcfccecac7dfd
Carter Hinsley <carterhinsley@gmail.com>
2015-08-07 10:46:07 -0400

    Fix Tael container top resizing

--- src/js/tael_sizing.js
+++ src/js/tael_sizing.js
@@ -1,10 +1,12 @@
 function resizeTaelContainer() {
-    $('.tael-container').css('bottom', 16);
+    var
+        header_bbox = $('.header')[0].getBoundingClientRect(),
+        tael_container = $('.tael-container');
+    tael_container.css('top', header_bbox.bottom);
+    tael_container.css('bottom', 16);
 }
 
 $(document).ready(function () {
-    var header_bbox = $('.header')[0].getBoundingClientRect();
-    $('.tael-container').css('top', header_bbox.bottom + 8);
     resizeTaelContainer();
     $(window).resize(resizeTaelContainer);
 });
