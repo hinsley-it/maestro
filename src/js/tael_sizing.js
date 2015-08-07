3686b790a2ad14deac5a82d0bbca7d2029f325d1
Carter Hinsley <carterhinsley@gmail.com>
2015-08-07 10:38:54 -0400

    Make Tael container resize height on document load

--- src/js/tael_sizing.js
+++ src/js/tael_sizing.js
@@ -5,5 +5,6 @@ function resizeTaelContainer() {
 $(document).ready(function () {
     var header_bbox = $('.header')[0].getBoundingClientRect();
     $('.tael-container').css('top', header_bbox.bottom + 8);
+    resizeTaelContainer();
     $(window).resize(resizeTaelContainer);
 });
