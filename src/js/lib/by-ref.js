afe1932db34385a768e83fb8b4861368217223dd
Carter Hinsley <carterhinsley@gmail.com>
2015-08-21 15:49:37 -0400

    Add by-ref.js library

---
+++ src/js/lib/by-ref.js
@@ -0,0 +1,32 @@
+/**
+ * by-ref.js
+ * Copyright (c) 2015 Carter Hinsley
+ * MIT License
+ */
+
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window).
+        root.returnExports = factory();
+  }
+}(this, function () {
+    exports = {};
+
+    exports.new = function (x) {
+        return [x];
+    };
+
+    exports.get = function (x) {
+        return x[0];
+    };
+
+    exports.set = function(x, new_value) {
+        return x[0] = new_value;
+    };
+}));
