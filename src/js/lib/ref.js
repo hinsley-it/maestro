4624f5674b6e5b8d5991a65d5999e276257c1de8
Carter Hinsley <carterhinsley@gmail.com>
2015-08-21 16:20:20 -0400

    Update ref.js

--- src/js/lib/ref.js
+++ src/js/lib/ref.js
@@ -4,29 +4,8 @@
  * MIT License
  */
 
-(function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD.
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node.
-        module.exports = factory();
-    } else {
-        // Browser globals (root is window).
-        root.returnExports = factory();
-  }
-}(this, function () {
-    exports = {};
+function ref(value) {
+    this.value = value;
+}
 
-    exports.new = function (x) {
-        return [x];
-    };
-
-    exports.get = function (x) {
-        return x[0];
-    };
-
-    exports.set = function(x, new_value) {
-        return x[0] = new_value;
-    };
-}));
+module.exports = ref;
